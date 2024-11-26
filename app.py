e503cedfdf0abcc74c151a28af0f46fa53e54a95
WouterDobber <72268241+WouterDobber@users.noreply.github.com>
2024-11-26 23:28:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,14 @@
 import os
-import wave
-import threading
-import numpy as np
-import sounddevice as sd
-
 from flask import Flask, render_template
 from flask_cors import CORS
+from audiorecorder import AudioRecorder
 
 app = Flask(__name__)
 CORS(app)
 
-# Audio recording variables
-RATE = 44100  # Sample rate
-CHANNELS = 1  # Mono
-CHUNK = 1024
-RECORD_SECONDS = 10
-recording = False
-frames = []
-record_thread = None
+recorder = None
+output_file = os.path.join(app.root_path, 'recording.wav')
+
 
 @app.route('/')
 def index():
@@ -26,47 +17,25 @@ def index():
 
 @app.route('/start-record', methods=['POST'])
 def start_record():
-    global recording, frames, record_thread
-
-    if recording:
+    global recorder
+    if recorder is not None and recorder.is_recording:
         return "Already recording!"
 
-    # Clear previous frames
-    frames = []
-    recording = True
-
-    # Start recording in a separate thread
-    def record_audio():
-        global frames, recording
-        while recording:
-            data = sd.rec(CHUNK, samplerate=RATE, channels=CHANNELS, dtype='int16')
-            sd.wait()  # Wait for the buffer to fill
-            frames.append(data)
-
-    record_thread = threading.Thread(target=record_audio)
-    record_thread.start()
+    # Start recording
+    recorder = AudioRecorder(output_file)
+    recorder.start_recording()
     return "Recording started"
 
 
 @app.route('/stop-record', methods=['POST'])
 def stop_record():
-    global recording, record_thread
-
-    if not recording:
+    global recorder
+    if recorder is None or not recorder.is_recording:
         return "Not recording!"
 
     # Stop recording
-    recording = False
-    record_thread.join()
-
-    # Save frames to a WAV file
-    output_file = os.path.join(app.root_path, 'recording.wav')
-    with wave.open(output_file, 'wb') as wf:
-        wf.setnchannels(CHANNELS)
-        wf.setsampwidth(2)  # 2 bytes for 'int16'
-        wf.setframerate(RATE)
-        wf.writeframes(b''.join(np.array(frames).flatten()))
-    
+    recorder.stop_recording()
+    recorder = None
     return f"Recording saved to {output_file}"
 
 
