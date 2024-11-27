f11528fb8ac2df8d3ab6602697fd084a2087b1ba
WouterDobber <72268241+WouterDobber@users.noreply.github.com>
2024-11-27 02:22:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -69,4 +69,5 @@ def upload():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from env or default to 5000
+    app.run(debug=True, host='0.0.0.0', port=port)
